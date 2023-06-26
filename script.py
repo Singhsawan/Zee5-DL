4245b6cd1d563bde99d7b986d55dffb6d90e54db
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-06-26 10:01:45 +0530

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ I'm A simple Zee5 link downloader bot With Permanent Thumbnail Support💯.
 
 Please send me any Zee5 link, I can upload it to telegram as File/Video.
 
-Currently I'm in beta mode 🥺, If found any bugs, report @NexonHex !
+Currently I'm in beta mode 🥺, If found any bugs, report @J_shree_ram !
 
 Click <i>/help</i> for more details....</b>"""
 
@@ -21,15 +21,15 @@ Click <i>/help</i> for more details....</b>"""
 NOTE: Download may take some time! So please wait for it to complete!"""
 
 
-    ABOUT_TEXT = """⭕️<b>My Name :Nexon's Zee5 DL</b>
+    ABOUT_TEXT = """⭕️<b>My Name :{}</b>
 
-⭕️<b>Creater :</b> @HexRidded
+⭕️<b>Creater :</b> @J_shree_ram
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
-⭕️<b>Source Code :</b> 👉 <a href='https://github.com/Hexridder/Zee5-Downloader'>Click Here</a>"""
+⭕️<b>Source Code :</b> 👉 <a href='@J_shree_ram'>Click Here</a>"""
 
 
 
@@ -41,7 +41,7 @@ NOTE: Download may take some time! So please wait for it to complete!"""
 <i>NOTE : Taking high resolutions may result in files above 2GB and hence cannot Upload to TG. So better select a medium resolution.</i> 😇
 """    
     
-    UPGRADE_TEXT = "PING at @NexonHex_grp"
+    UPGRADE_TEXT = "PING at @J_shree_ram"
     
     DOWNLOAD_START = "Trying to download to my server. This may take a while 😴"
     
@@ -55,11 +55,11 @@ NOTE: Download may take some time! So please wait for it to complete!"""
     
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom Thumbnail cleared succesfully."
     
-    SHOW_THUMB = "@TroJanzHEX\n\nUse /delthumb to clear this thumbnail."
+    SHOW_THUMB = "@J_shree_ram\n\nUse /delthumb to clear this thumbnail."
     
     NO_THUMB = "SED😕 No saved thumbnails Found!!"
     
-    CUSTOM_CAPTION_UL_FILE = "<b>{newname}\n\n©️ @NexonHex</b>"
+    CUSTOM_CAPTION_UL_FILE = "<b>{newname}\n\n©️ @J_shree_ram</b>"
     
     TIMEOUT = "<b><i>Sorry for the delay. It'll help reduce the flood wait</i> 😇\n\nWait for {} sec and try again.</b>"
     
